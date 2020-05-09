2b18d823cfd60296eb5730a4711b535b26f70116
fallenoak <git@fallenoak.me>
2020-05-08 22:52:29 -0500

    chore: wire up react linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,27 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
   },
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: '2018',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:prettier/recommended',
   ],
   rules: {
