8dbd05279a8e87c4207e95e464f47d6606284cf5
fallenoak <git@fallenoak.me>
2020-05-16 15:30:21 -0500

    chore: add type util

---
+++ src/util/type.ts
@@ -0,0 +1,10 @@
+type SetDifference<A, B> = A extends B ? never : A;
+
+type SetComplement<A, A1 extends A> = SetDifference<A, A1>;
+
+type Subtract<T extends T1, T1 extends object> = Pick<
+  T,
+  SetComplement<keyof T, keyof T1>
+>;
+
+export { Subtract };
